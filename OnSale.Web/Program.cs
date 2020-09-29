ceb1f6f733e523b9372e294993499a0668f4ddc2
Emilio Barrea <barrera_emilio@yahoo.es>
2020-09-29 23:41:54 +0100

    All About SeedDB

--- OnSale.Web/Program.cs
+++ OnSale.Web/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OnSale.Web.Data;
 
 namespace OnSale.Web
 {
@@ -13,14 +10,29 @@ namespace OnSale.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static void RunSeeding(IWebHost host)
+        {
+            IServiceScopeFactory scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+                SeedDB seeder = scope.ServiceProvider.GetService<SeedDB>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+
+            //return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            //{
+            //    webBuilder.UseStartup<Startup>();
+            //});
+        }
     }
 }
--- OnSale.Web/Startup.cs
+++ OnSale.Web/Startup.cs
@@ -26,6 +26,8 @@ namespace OnSale.Web
             {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
