0c505b71c99a0bccd68b7c59401d46843a9ca52a
Emilio Barrea <barrera_emilio@yahoo.es>
2020-10-06 13:18:50 +0100

    entites categpry, products and ImagenProduct olso caegppry Control and view category all this for Upload Images

---
+++ OnSale.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnSale.Web.Data;
+
+namespace OnSale.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public CategoriesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Categories.ToListAsync());
+        }
+    }
+}
--- OnSale.Web/Data/DataContext.cs
+++ OnSale.Web/Data/DataContext.cs
@@ -10,15 +10,20 @@ namespace OnSale.Web.Data
 
         }
 
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<City> Cities { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Country>().HasIndex(t => t.Name).IsUnique();
             modelBuilder.Entity<Department>().HasIndex(d => d.Name).IsUnique();
             modelBuilder.Entity<City>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(p => p.Name).IsUnique();
 
 
         }
